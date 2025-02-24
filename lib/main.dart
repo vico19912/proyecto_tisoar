683e99589ddfa34a3d7c416845b12cd4dd59c86b
Victor Corea <vico19912@gmail.com>
2025-02-23 20:40:23 -0600

    Se agrego el archivo para la pantalla de login

--- lib/main.dart
+++ lib/main.dart
@@ -46,15 +46,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
 
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
